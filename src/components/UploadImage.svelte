00bf8451d24b6d9aa43fbf5183c7e2e975d73662
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-02-24 18:41:24 -0300

    Create an upload image component

---
+++ src/components/UploadImage.svelte
@@ -0,0 +1,17 @@
+<script ln="ts">
+  import {Cloudinary} from '@cloudinary/url-gen'
+  import {Resize} from '@cloudinary/url-gen/actions'
+
+</script>
+
+<div>
+  <form>
+    <button>Upload file</button>
+  </form>
+</div>
+
+<style>
+  form {
+    padding: 2em;
+  }
+</style>
