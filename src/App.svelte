eac2c0cb034bff49910c904e90c8dbf9c23a95f4
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-02-23 23:52:49 -0300

    Import the cloudinary logo component as credits for the service provided

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,31 @@
 
   import Header from './components/header.svelte'
   import Footer from './components/footer.svelte'
+  import CloudinaryLogo from './components/CloudinaryLogo.svelte';
 </script>
 
 <main>
   <Header />
+  <div class="credits">
+    <small>Thanks to </small>
+    <a href="https://cloudinary.com" target="_blank" rel="noreferrer"><CloudinaryLogo /></a>
+  </div>
   <h1>Customize your image!</h1>
   <Footer />
 </main>
 
 <style>
+  small {
+    font-style: bold;
+  }
 
+  .credits {
+    margin-top: 2em;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
