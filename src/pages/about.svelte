a886726da2a5a288d32448a2c29aeb50c5633499
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-02-27 23:56:41 -0300

    Create a pages folder

---
+++ src/pages/about.svelte
@@ -0,0 +1,5 @@
+<section>
+  <img src="" alt="Yo" />
+  <h1>Elias Pereyra</h1>
+  <p>Here a brief description about me and what stuff I'm messing with</p>
+</section>
---
+++ src/pages/home.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Footer from '../components/footer.svelte'
+  import UploadImage from '../components/UploadImage.svelte';
+  import CloudinaryLogo from '../components/CloudinaryLogo.svelte';
+</script>
+
+
+  <div class="credits">
+    <small>Thanks to </small>
+    <a href="https://cloudinary.com" target="_blank" rel="noreferrer"><CloudinaryLogo /></a>
+  </div>
+  <h1>Customize your image!</h1>
+  <UploadImage />
+  <Footer />
+
+  <style>
+    small {
+      font-style: bold;
+    }
+  
+    .credits {
+      margin-top: 2em;
+  
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
