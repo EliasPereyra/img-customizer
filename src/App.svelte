6ba855637d5d87d3d11a47256812dd264f91fa26
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-02-28 21:34:23 -0300

    Import the svelte routing components and set up the paths

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,22 @@
 <script lang="ts">
-
+  import {Router, Route, Link} from "svelte-routing"
+  import Footer from "./components/footer.svelte";
+  
   import Header from './components/header.svelte'
-  import Footer from './components/footer.svelte'
-  import UploadImage from './components/UploadImage.svelte';
-  import CloudinaryLogo from './components/CloudinaryLogo.svelte';
+  import Contact from "./pages/contact.svelte";
+  import Home from "./pages/home.svelte";
+  import NotFound from "./pages/NotFound.svelte";
+
 </script>
 
 <main>
-  <Header />
-  <div class="credits">
-    <small>Thanks to </small>
-    <a href="https://cloudinary.com" target="_blank" rel="noreferrer"><CloudinaryLogo /></a>
-  </div>
-  <h1>Customize your image!</h1>
-  <UploadImage />
-  <Footer />
+  <Router>
+    <Header />
+    <Route path="/" component={Home} />
+    <Route path="contact" component="{Contact}" />
+    <Route component={NotFound} />
+    <Footer />
+  </Router>
 </main>
 
-<style>
-  small {
-    font-style: bold;
-  }
 
-  .credits {
-    margin-top: 2em;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-</style>
