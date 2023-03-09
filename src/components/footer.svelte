e5d5cb4fbc16b7d6b6c8056dd1d52af2d70e4144
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-03-08 23:46:02 -0300

    Add a background color for the foot component

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,3 +1,12 @@
 <footer>
+  <p>No Copyright 2023. Not All Rights Reserved.</p>
   <p>Here's the <a href="/">repo</a>, check it out</p>
 </footer>
+
+<style>
+  footer {
+    background-color: #222;
+    padding: 2em;
+    font-size: .9rem;
+  }
+</style>
